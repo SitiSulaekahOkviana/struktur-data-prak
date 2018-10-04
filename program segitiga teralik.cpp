4b02311accaa3e4671f5d0b6af15700bc3582160
SitiSulaekahOkviana <43657052+SitiSulaekahOkviana@users.noreply.github.com>
2018-10-04 10:24:55 +0700

    Add files via upload

---
+++ program segitiga teralik.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main (){
+	
+	string k;
+	cout<<"Masukkan suatu kata: ";
+	cin>>k;
+	cout<<endl;
+	int i=k.length()-1;
+	for (i;  i>=0; i--){
+		for (int j=0; j<=i;j++){
+			cout<<k[j];
+		}
+		cout<<endl;
+		}
+	
+}
